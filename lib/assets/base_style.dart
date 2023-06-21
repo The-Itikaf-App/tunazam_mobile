718235272cdbd1c1b52ee8228dc47f8bec3e4b9d
Muktar Yusuf <mayunited12@yahoo.com>
2023-06-21 11:50:39 +0100

    introduce assets library

---
+++ lib/assets/base_style.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+
+Color primary = const Color(0xFFFFFFFF);
+
+class Styles {
+  static Color primaryColor = primary;
+}
---
+++ lib/assets/data.dart
@@ -0,0 +1,10 @@
+List<Map<String, dynamic>> feedData = [
+  {
+    "title": "Mainstream 1",
+    "description": "Mainstream 1 description",
+  },
+  {
+    "title": "Data Stream 1",
+    "description": "Data Stream 1 description",
+  },
+];
